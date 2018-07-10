41e0ae5d6ce9f3a220c9957807478776bd2de815
vlad <vladbespalov@yahoo.com>
2018-07-10 11:46:35 +0300

    Убрал личные данные из почты

--- 123.js
+++ 123.js
@@ -1,35 +1,35 @@
-﻿var fs = require('fs');
+﻿// var fs = require('fs');
 
-var text='qweasodjawiudhasdyehy8awdasiudjaoidyushasy';
-var news = 'рашка отлетела по пенальти';
+// var text='qweasodjawiudhasdyehy8awdasiudjaoidyushasy';
+// var news = 'рашка отлетела по пенальти';
 
 
-var cont =  fs.writeFileSync('nod.txt',text)
-var newasd = fs.readFileSync('nod.txt','utf-8');
-console.log(newasd);
-var copyFile = fs.copyFileSync('nod.txt','copy.txt')
-var asdasd = fs.readFileSync ('copy.txt','utf-8')
-console.log(asdasd);
-// var del = fs.unlinkSync('copy.txt');
+// var cont =  fs.writeFileSync('nod.txt',text)
+// var newasd = fs.readFileSync('nod.txt','utf-8');
+// console.log(newasd);
+// var copyFile = fs.copyFileSync('nod.txt','copy.txt')
 // var asdasd = fs.readFileSync ('copy.txt','utf-8')
 // console.log(asdasd);
-fs.appendFileSync('news.txt',news);
-var newsread = fs.readFileSync('news.txt','utf-8')
-console.log(newsread)
+// // var del = fs.unlinkSync('copy.txt');
+// // var asdasd = fs.readFileSync ('copy.txt','utf-8')
+// // console.log(asdasd);
+// fs.appendFileSync('news.txt',news);
+// var newsread = fs.readFileSync('news.txt','utf-8')
+// console.log(newsread)
 
-// var nodemailer = require('nodemailer');
-// var transporter = nodemailer.createTransport({
-//     service: 'Gmail',
-//     auth: {
-//         user: 'vlad.bespalov23',
-//         pass: '23071995'
-//     }
-// });
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'log',
+        pass: 'pass'
+    }
+});
 
-// console.log('created');
-// transporter.sendMail({
-// from: 'vlad.bespalov23@gmail.com',
-//   to: 'vladbespalov@yahoo.com',
-//   subject: 'hello world!',
-//   text: 'hello world!'
-// });
+console.log('created');
+transporter.sendMail({
+from: 'my@gmail.com',
+  to: 'my2@gmail.com',
+  subject: 'hello world!',
+  text: 'hello world!'
+});
